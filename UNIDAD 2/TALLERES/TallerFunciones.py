11a6d743fbff990a541e15d7149120d1a1b60225
Juan Sebastian Velasquez Ruiz <sebasvr2017@gmail.com>
2022-06-01 17:11:15 -0500

    Subida toda la unidad 2

---
+++ UNIDAD 2/TALLERES/TallerFunciones.py
@@ -0,0 +1,47 @@
+import Taller 
+
+# EJERCICIO 1
+nom = input("Digite su nombre: ")
+print(Taller.ejercicio_1(nom))
+
+# EJERCICIO 2
+num = int(input("Digite el nuemro que desea conocer el factorial: "))
+print(Taller.ejercicio_2(num))
+
+# EJERCICIO 3 
+fact = int(input("Digite el valor de su factura: "))
+impuesto = int(input("Digite el porcentaje del iva: "))
+print(Taller.ejercicio_3(fact, impuesto))
+
+# EJERCICIO 4
+rad = int(input("Digite el radio de su circulo: "))
+print(Taller.ejercicio_4_circulo(rad))
+
+altu = int(input("Digite la altura de su cilindro: "))
+print(Taller.ejercicio_4_cilindro(altu))
+
+# EJERCICIO 5
+num = int(input("Digite un numero entero: "))
+print(Taller.ejercicio_5(num))
+
+#EJERCICIO 6
+pes = int(input("Digite su peso: "))
+estat = int(input("Digite su estatura: "))
+print(Taller.ejercicio_6(pes,estat))
+
+# EJERCICIO 7
+paya = int(input("Digite la cantidad de payasos: "))
+muñe = int(input("Digite la cantidad de muñecas: "))
+print(Taller.ejercicio_7(paya,muñe))
+
+# EJERCICIO 8
+pan = int(input("Digite el numero de barras de pan: "))
+print(Taller.ejercicio_8(pan))
+
+# EJERCICIO 9
+cant = int(input("Digite la cantidad ingresada a la cuenta de ahorros: "))
+print(Taller.ejercicio_9(cant))
+
+# EJERCICIO 10
+segun = int(input("Digite los segundos que desea convertir: "))
+print(Taller.ejercicio_10(segun))
